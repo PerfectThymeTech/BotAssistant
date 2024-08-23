b18e274dcdf23f7387bcfc660fb258be22625cb5
Marvin Buss <marvin.buss@gmail.com>
2024-08-23 12:59:44 +0200

    Test AOAI deployment

--- code/infra/aoai.tf
+++ code/infra/aoai.tf
@@ -15,18 +15,17 @@ module "azure_open_ai" {
   cognitive_account_firewall_bypass_azure_services        = false
   cognitive_account_outbound_network_access_restricted    = true
   cognitive_account_outbound_network_access_allowed_fqdns = []
-  cognitive_account_deployments                           = {}
-  # {
-  #   gpt-4o = {
-  #     model_name     = "gpt-4o"
-  #     model_version  = "2024-05-13"
-  #     scale_type     = null
-  #     scale_tier     = null
-  #     scale_size     = null
-  #     scale_family   = null
-  #     scale_capacity = 100
-  #   }
-  # }
+  cognitive_account_deployments                           = {
+    gpt-4o = {
+      model_name     = "gpt-4o"
+      model_version  = "2024-05-13"
+      scale_type     = "Standard"
+      scale_tier     = null
+      scale_size     = null
+      scale_family   = null
+      scale_capacity = 100
+    }
+  }
   diagnostics_configurations            = []
   subnet_id                             = "/subscriptions/8f171ff9-2b5b-4f0f-aed5-7fa360a1d094/resourceGroups/tfmdltst-dev-rg/providers/Microsoft.Network/virtualNetworks/tfmdltst-dev-vnet/subnets/PrivateEndpoints"
   connectivity_delay_in_seconds         = 0
@@ -34,17 +33,17 @@ module "azure_open_ai" {
   customer_managed_key                  = null
 }
 
-resource "azurerm_cognitive_deployment" "cognitive_deployment_gpt_4o" {
-  name                 = "gpt-4o"
-  cognitive_account_id = module.azure_open_ai.cognitive_account_id
+# resource "azurerm_cognitive_deployment" "cognitive_deployment_gpt_4o" {
+#   name                 = "gpt-4o"
+#   cognitive_account_id = module.azure_open_ai.cognitive_account_id
 
-  model {
-    format  = "OpenAI"
-    name    = "gpt-4o"
-    version = "2024-05-13"
-  }
-  scale {
-    capacity = 100
-    type     = "Standard"
-  }
-}
+#   model {
+#     format  = "OpenAI"
+#     name    = "gpt-4o"
+#     version = "2024-05-13"
+#   }
+#   scale {
+#     capacity = 100
+#     type     = "Standard"
+#   }
+# }

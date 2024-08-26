a0736ab1e5e2f20e3bc2068108d2af98a455cf32
Marvin Buss <marvin.buss@gmail.com>
2024-08-26 19:08:18 +0200

    Test baseline

--- code/backend/utils.py
+++ code/backend/utils.py
@@ -16,25 +16,25 @@ def enable_logging():
     # Configure azure monitor logs
     configure_azure_monitor(
         connection_string=settings.APPLICATIONINSIGHTS_CONNECTION_STRING,
-        instrumentation_options={
-            "azure_sdk": {"enabled": True},
-            "django": {"enabled": False},
-            "fastapi": {"enabled": False},
-            "flask": {"enabled": True},
-            "psycopg2": {"enabled": False},
-            "requests": {"enabled": False},
-            "urllib": {"enabled": False},
-            "urllib3": {"enabled": False},
-        },
-        resource=Resource.create(
-            {
-                "service.name": settings.SERVER_NAME,
-                "service.namespace": settings.PROJECT_NAME,
-                "service.instance.id": settings.WEBSITE_INSTANCE_ID,
-            }
-        ),
-        logger_name=settings.PROJECT_NAME,
-        enable_live_metrics=True,
+        # instrumentation_options={
+        #     "azure_sdk": {"enabled": True},
+        #     "django": {"enabled": False},
+        #     "fastapi": {"enabled": False},
+        #     "flask": {"enabled": True},
+        #     "psycopg2": {"enabled": False},
+        #     "requests": {"enabled": False},
+        #     "urllib": {"enabled": False},
+        #     "urllib3": {"enabled": False},
+        # },
+        # resource=Resource.create(
+        #     {
+        #         "service.name": settings.SERVER_NAME,
+        #         "service.namespace": settings.PROJECT_NAME,
+        #         "service.instance.id": settings.WEBSITE_INSTANCE_ID,
+        #     }
+        # ),
+        # logger_name=settings.PROJECT_NAME,
+        # enable_live_metrics=True,
     )
 
     # Enable additional instrumentation

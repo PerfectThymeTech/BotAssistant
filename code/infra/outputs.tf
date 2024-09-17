63e43f8420d611a18206d584d426ee00d0fbfa04
Marvin Buss <marvin.buss@gmail.com>
2024-09-17 14:59:07 +0200

    Add bot service id output

--- code/infra/outputs.tf
+++ code/infra/outputs.tf
@@ -3,3 +3,9 @@ output "bot_service_name" {
   value       = reverse(split(module.bot_service.bot_service_id, "/"))[0]
   sensitive   = false
 }
+
+output "bot_service_id" {
+  description = "Specifies the resource id of the bot service."
+  value       = module.bot_service.bot_service_id
+  sensitive   = false
+}

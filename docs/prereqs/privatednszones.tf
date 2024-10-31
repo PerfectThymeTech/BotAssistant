ca10bfbf16c7b68c4d837d0bfe36108ac1306c0b
Marvin Buss <marvin.buss@gmail.com>
2024-10-31 11:50:32 +0100

    Update private dns link config

--- docs/prereqs/privatednszones.tf
+++ docs/prereqs/privatednszones.tf
@@ -9,9 +9,9 @@ resource "azurerm_private_dns_zone_virtual_network_link" "private_dns_zone_virtu
   for_each = local.private_dns_zone_names
 
   name                = "${each.key}-${azurerm_virtual_network.virtual_network.name}"
-  resource_group_name = azurerm_resource_group.resource_group.name
+  resource_group_name = azurerm_private_dns_zone.private_dns_zone[each.key].resource_group_name
   tags                = var.tags
 
-  private_dns_zone_name = each.value
+  private_dns_zone_name = azurerm_private_dns_zone.private_dns_zone[each.key].name
   virtual_network_id    = azurerm_virtual_network.virtual_network.id
 }

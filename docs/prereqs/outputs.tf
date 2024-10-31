80606a79f8654e92c66d6ae522e7810b9debd69b
Marvin Buss <marvin.buss@gmail.com>
2024-10-31 11:50:04 +0100

    Update sensitivity of outputs

--- docs/prereqs/outputs.tf
+++ docs/prereqs/outputs.tf
@@ -1,30 +1,30 @@
 output "virtual_network_id" {
   description = "Specifies the id of the virtual network."
-  sensitive   = true
+  sensitive   = false
   value       = azurerm_virtual_network.virtual_network.id
 }
 
 output "route_table_id" {
   description = "Specifies the id of the route table."
-  sensitive   = true
+  sensitive   = false
   value       = azurerm_route_table.route_table.id
 }
 
 output "network_security_group_id" {
   description = "Specifies the id of the network security group."
-  sensitive   = true
+  sensitive   = false
   value       = azurerm_network_security_group.network_security_group.id
 }
 
 output "log_analytics_workspace_id" {
   description = "Specifies the id of the log analytics workspace."
-  sensitive   = true
+  sensitive   = false
   value       = module.log_analytics_workspace.log_analytics_workspace_id
 }
 
 output "private_dns_zone_ids" {
   description = "Specifies the ids of the private dns zones."
-  sensitive   = true
+  sensitive   = false
   value = [
     for key, value in local.private_dns_zone_names :
     azurerm_private_dns_zone.private_dns_zone[key].id

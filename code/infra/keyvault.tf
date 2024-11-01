fef0a3bebae4a8aab81e2e1f2dd1a0864135e80f
Marvin Buss <marvin.buss@gmail.com>
2024-11-01 07:52:48 +0100

    Fix bug in Key Vault

--- code/infra/keyvault.tf
+++ code/infra/keyvault.tf
@@ -25,7 +25,7 @@ module "key_vault_ingestion" {
   }
 
   location                             = var.location
-  resource_group_name                  = azurerm_resource_group.resource_group_consumption.name
+  resource_group_name                  = azurerm_resource_group.resource_group_ingestion.name
   tags                                 = var.tags
   key_vault_name                       = "${local.prefix}-ngst-kv001"
   key_vault_sku_name                   = "standard"

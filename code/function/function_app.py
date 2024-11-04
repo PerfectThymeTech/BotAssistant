cc669941369551c419b6a344a9cad29b536950d7
Marvin Buss <marvin.buss@gmail.com>
2024-11-05 00:13:44 +0100

    Update function name

--- code/function/function_app.py
+++ code/function/function_app.py
@@ -1,9 +1,10 @@
 import azure.functions as func
-from blobfiletrigger.function import bp as bp_videoupload
+
+# from blobfiletrigger.function import bp as bp_blobfiletrigger
 from health.function import bp as bp_health
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.FUNCTION)
 
 # Register blueprints
 app.register_functions(bp_health)
-app.register_functions(bp_videoupload)
+# app.register_functions(bp_blobfiletrigger)
